c58d132cd50c0291522fe97b1c23706394a36c98
Giovanni Mascarino <giovanni.mascarino@studenti.polito.it>
2022-03-30 01:30:46 +0200

    Changed saved files folder with trial naming and timing

--- Core/parameters.py
+++ Core/parameters.py
@@ -1,3 +1,6 @@
+import os
+import time
+
 ########### GLOBAL VARIABLES ###########
 #### SET FIND BEST CONDITION at the beginning
 set_latency_or_snr = 'SNR' # 'SNR' or 'latency' for find best
@@ -44,6 +47,9 @@ file_nodes_full = root / 'Resources' / 'Lab_Networks' / 'nodes_full.json'
 file_nodes_not_full = root / 'Resources' / 'Lab_Networks' / 'nodes_not_full.json'
 
 ### Results saved
+trial_path = 'best_' + set_latency_or_snr + '_of_' + time.strftime('%Y_%m_%d__%H_%M_%S')
+# if not os.path.isdir('../Results/Lab9/' + trial_path):  # if Results doesn't exists, it creates it
+    # os.makedirs('../Results/Lab9/' + trial_path)
 ## LAB 8
 lab8_fig1 = root / 'Results' / 'Lab8' / 'lab8_fig1_node_couples_full_and_not_full.png'
 lab8_fig2 = root / 'Results' / 'Lab8' / 'lab8_fig2_node_couples_transceiver.png'
@@ -52,20 +58,20 @@ lab8_fig4 = root / 'Results' / 'Lab8' / 'lab8_fig4_SNR_transceiver.png'
 lab8_fig5 = root / 'Results' / 'Lab8' / 'lab8_fig5_bit_rates.png'
 ## LAB 9
 # point 5
-lab9_fig1 = root / 'Results' / 'Lab9' / ('lab9_fig1_point5_IO_best_' + set_latency_or_snr)
-lab9_fig2 = root / 'Results' / 'Lab9' / ('lab9_fig2_point5_SNR_best_' + set_latency_or_snr)
-lab9_fig3 = root / 'Results' / 'Lab9' / ('lab9_fig3_point5_Rb_best_' + set_latency_or_snr)
+lab9_fig1 = root / 'Results' / 'Lab9' / trial_path / ('lab9_fig1_point5_IO_best_' + set_latency_or_snr)
+lab9_fig2 = root / 'Results' / 'Lab9' / trial_path / ('lab9_fig2_point5_SNR_best_' + set_latency_or_snr)
+lab9_fig3 = root / 'Results' / 'Lab9' / trial_path / ('lab9_fig3_point5_Rb_best_' + set_latency_or_snr)
 # point 7
 # SNR
-lab9_fig4 = root / 'Results' / 'Lab9' / ('lab9_fig4_point7_SNR_fixed_best_' + set_latency_or_snr)
-lab9_fig5 = root / 'Results' / 'Lab9' / ('lab9_fig5_point7_SNR_flex_best_' + set_latency_or_snr)
-lab9_fig6 = root / 'Results' / 'Lab9' / ('lab9_fig6_point7_SNR_shannon_best_' + set_latency_or_snr)
+lab9_fig4 = root / 'Results' / 'Lab9' / trial_path / ('lab9_fig4_point7_SNR_fixed_best_' + set_latency_or_snr)
+lab9_fig5 = root / 'Results' / 'Lab9' / trial_path / ('lab9_fig5_point7_SNR_flex_best_' + set_latency_or_snr)
+lab9_fig6 = root / 'Results' / 'Lab9' / trial_path / ('lab9_fig6_point7_SNR_shannon_best_' + set_latency_or_snr)
 # BITRATE
-lab9_fig7 = root / 'Results' / 'Lab9' / ('lab9_fig7_point7_Rb_fixed_best_' + set_latency_or_snr)
-lab9_fig8 = root / 'Results' / 'Lab9' / ('lab9_fig8_point7_Rb_flex_best_' + set_latency_or_snr)
-lab9_fig9 = root / 'Results' / 'Lab9' / ('lab9_fig9_point7_Rb_shannon_best_' + set_latency_or_snr)
+lab9_fig7 = root / 'Results' / 'Lab9' / trial_path / ('lab9_fig7_point7_Rb_fixed_best_' + set_latency_or_snr)
+lab9_fig8 = root / 'Results' / 'Lab9' / trial_path / ('lab9_fig8_point7_Rb_flex_best_' + set_latency_or_snr)
+lab9_fig9 = root / 'Results' / 'Lab9' / trial_path / ('lab9_fig9_point7_Rb_shannon_best_' + set_latency_or_snr)
 # LAT
-lab9_fig10 = root / 'Results' / 'Lab9' / ('lab9_fig10_point7_latency_fixed_best_' + set_latency_or_snr)
-lab9_fig11 = root / 'Results' / 'Lab9' / ('lab9_fig11_point7_latency_flex_best_' + set_latency_or_snr)
-lab9_fig12 = root / 'Results' / 'Lab9' / ('lab9_fig12_point7_latency_shannon_best_' + set_latency_or_snr)
+lab9_fig10 = root / 'Results' / 'Lab9' / trial_path / ('lab9_fig10_point7_latency_fixed_best_' + set_latency_or_snr)
+lab9_fig11 = root / 'Results' / 'Lab9' / trial_path / ('lab9_fig11_point7_latency_flex_best_' + set_latency_or_snr)
+lab9_fig12 = root / 'Results' / 'Lab9' / trial_path / ('lab9_fig12_point7_latency_shannon_best_' + set_latency_or_snr)
 ##############################################################################################
